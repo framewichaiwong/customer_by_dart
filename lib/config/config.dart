d6915a78e1f865ffea9226e9cd696133191c339c
framewichaiwong <33681288+framewichaiwong@users.noreply.github.com>
2022-01-12 04:36:28 +0700

    customer can add image slip

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,6 +1,6 @@
 class Config {
   //static String? url = "https://restaurant-repository.herokuapp.com";
-  static String url = "http://192.168.1.62:8080";
+  // static String url = "http://192.168.1.62:8080";
   //static String url = "https://iotboot.com";
-  // static String url = "http://10.0.2.2:8080";
+  static String url = "http://10.0.2.2:8080";
 }
